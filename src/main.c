b91ee12e6f8b5a65490c9a21eabb02bee3cd1342
Trond F. Christiansen <trochr@hotmail.com>
2024-01-16 14:26:56 +0100

    Coded networking ping-pong

--- src/main.c
+++ src/main.c
@@ -3,31 +3,44 @@
 #include <unistd.h>
 #include "sverresnetwork.h"
 
+
+// LAB udp server ip: 10.100.23.129
+char * server_ip = "10.100.23.129";
+
 void udpmessageReceived(const char * ip, char * data, int datalength){
 
   // Assuming an ascii string here - a binary blob (including '0's) will
   // be ugly/truncated.
   printf("Received UDP message from %s: '%s'\n",ip,data);
+  if(data == "ping"){
+    sleep(1000);
+    udp_send(server_ip, 20014,"pong",5);
+  }
+  else if (data == "pong")
+  {
+    sleep(1000);
+    udp_send(server_ip,20014,"ping",5);
+  }
+  
 }
 
-/*void udpmessageReceived(const char * ip, int port, char * data, int datalength){
-
-  printf("Received UDP message from %s:%d: '%s'\n",ip,port,data);
-}  */
-
-int main(){
+int main(int argc, char * argv[]){
   char * ip = getMyIpAddress("enp0s31f6");
   printf("My Ip is %s\n",ip);
-
-
-  udp_startReceiving(30000,udpmessageReceived);
-    // LAB udp server ip: 10.100.23.129
-  char * server_ip = "10.100.23.129";
-
-  udp_send(server_ip,20014,"Hello World",12);
-  sleep(100);
-
   free(ip);
+  if(argc >= 2){
+    server_ip = argv[1];
+  }
+
+  udp_startReceiving(20014,udpmessageReceived);
+  sleep(100); // wait for recieve to start
+  if(argc >= 3){
+    if(argv[2]== "start"){
+      printf("Starting ping-pong");
+      udp_send(server_ip,20014,"ping",5);
+    }
+  }
+
 
   return 0;
 }
