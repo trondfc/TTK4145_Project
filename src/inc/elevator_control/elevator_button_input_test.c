a6961bb60206ddf0a82c57379b8ff4a9994bbb06
Trond F. Christiansen <trochr@hotmail.com>
2024-01-25 15:36:59 +0100

    fixed latching button press in elevator button struct

--- src/inc/elevator_control/elevator_button_input_test.c
+++ src/inc/elevator_control/elevator_button_input_test.c
@@ -11,6 +11,14 @@ pthread_mutex_t elevator_struct_mutex;
 
 struct elevator_floor_button_inputs_t* elevator_struct;
 
+void clear_elevator_struct(){
+    for(int floor = 0; floor < N_FLOORS; floor++){
+        elevator_struct->floor_inputs[floor].request_up = 0;
+        elevator_struct->floor_inputs[floor].request_down = 0;
+        elevator_struct->floor_inputs[floor].request_cabin = 0; 
+    }
+}
+
 void* print_elevator_struct(){
     while(1){
         pthread_mutex_lock(&elevator_struct_mutex);
@@ -19,14 +27,10 @@ void* print_elevator_struct(){
             printf("Floor %d:\t", floor);
             printf("Request up: %d\t", elevator_struct->floor_inputs[floor].request_up);
             printf("Request down: %d\t", elevator_struct->floor_inputs[floor].request_down);
-            printf("Request cabin: %d\t", elevator_struct->floor_inputs[floor].request_cabin);
+            printf("Request cabin: %d\n", elevator_struct->floor_inputs[floor].request_cabin);
         }
         printf("Cleraring struct\n");
-        for(int floor = 0; floor < N_FLOORS; floor++){
-            elevator_struct->floor_inputs[floor].request_up = 0;
-            elevator_struct->floor_inputs[floor].request_down = 0;
-            elevator_struct->floor_inputs[floor].request_cabin = 0; 
-        }
+        clear_elevator_struct();
         pthread_mutex_unlock(&elevator_struct_mutex);
         sleep(5);
     }
@@ -42,6 +46,12 @@ void* poll_elevator_buttons_wrapper(){
 }
 
 int main(){
+    clear_elevator_struct();    // Dont need to lock mutex here, since no other thread is running yet
+    
+    elevator_hardware_init();
+
+    elevator_struct = malloc(sizeof(struct elevator_floor_button_inputs_t));
+
     if(pthread_mutex_init(&elevator_struct_mutex, NULL) != 0){
         printf("Mutex init failed\n");
         exit(1);
--- src/inc/elevator_control/elevator_button_inputs.c
+++ src/inc/elevator_control/elevator_button_inputs.c
@@ -2,15 +2,18 @@
 
 struct elevator_button_inputs_t poll_elevator_buttons_pressed(int floor){
     struct elevator_button_inputs_t button_inputs;
-    button_inputs.request_up |= elevator_hardware_get_button_signal(BUTTON_CALL_UP, floor);
-    button_inputs.request_down |= elevator_hardware_get_button_signal(BUTTON_CALL_DOWN, floor);
-    button_inputs.request_cabin |= elevator_hardware_get_button_signal(BUTTON_COMMAND, floor);
+    button_inputs.request_up = elevator_hardware_get_button_signal(BUTTON_CALL_UP, floor);
+    button_inputs.request_down = elevator_hardware_get_button_signal(BUTTON_CALL_DOWN, floor);
+    button_inputs.request_cabin = elevator_hardware_get_button_signal(BUTTON_COMMAND, floor);
     return button_inputs;
 }
 
 void poll_elevator_floor_buttons_pressed(struct elevator_floor_button_inputs_t* floor_button_inputs){
     for(int floor = 0; floor < N_FLOORS; floor++){
-        floor_button_inputs->floor_inputs[floor] = poll_elevator_buttons_pressed(floor);
+        struct elevator_button_inputs_t floor_inputs = poll_elevator_buttons_pressed(floor);
+        floor_button_inputs->floor_inputs[floor].request_up |= floor_inputs.request_up;
+        floor_button_inputs->floor_inputs[floor].request_down |= floor_inputs.request_down;
+        floor_button_inputs->floor_inputs[floor].request_cabin |= floor_inputs.request_cabin;
     }
 }
 
