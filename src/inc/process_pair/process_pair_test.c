9a3a47380f0988a9680f3e93f61fc193268bda2d
Trond F. Christiansen <trochr@hotmail.com>
2024-02-27 11:50:40 +0100

    Added process pair lib

---
+++ src/inc/process_pair/process_pair_test.c
@@ -0,0 +1,21 @@
+/**
+ * @file process_pair_test.c
+ * @author Trondfc ()
+ * @brief test file for process_pair module
+ * @version 0.1
+ * @date 2024-02-27
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+#include "process_pair.h"
+
+int main(){
+    sysQueInit(5); //start the system queue reset system, timeout 5 seconds
+    while (1)
+    {
+        /* code */
+    }
+    
+    return 0;
+}
