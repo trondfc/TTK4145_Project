3e7b6f8751b6915cee54a4152ffe4de8b162047a
Syver <Syverharaldsen@gmail.com>
2024-02-27 14:52:10 +0100

    Bug fixes

--- src/inc/elevator_control/elevator_button_inputs.c
+++ src/inc/elevator_control/elevator_button_inputs.c
@@ -5,14 +5,14 @@ void poll_new_orders(elevator_hardware_info_t *elevator, order_queue_t *queue){
     for(uint8_t i = 0; i < N_FLOORS; i++){
         for(uint8_t j = 0; j < N_BUTTONS; j++){
             if(elevator_hardware_get_button_signal(j, i, elevator)){
-                order_event_t order;
-                strcpy(order.elevator_id, elevator->ip);
-                order.floor = i;
-                order.order_type = j;
-                order.order_status = RECIVED;
-                time(&order.timestamp);
-                order.order_id = GenerateOrderID(&order);
-                enqueue_order(queue, &order);
+                order_event_t * order = (order_event_t *)malloc(sizeof(order_event_t));
+                strcpy(order->elevator_id, elevator->ip);
+                order->floor = i;
+                order->order_type = j;
+                order->order_status = RECIVED;
+                time(order->timestamp);
+                order->order_id = GenerateOrderID(order);
+                enqueue_order(queue, order);
             }
         }
     }
