72eb7c2bfa75c88abff6d4951186a5a6de94559f
Syver <Syverharaldsen@gmail.com>
2024-03-04 17:03:52 +0100

    Added int return on poll buttons

--- src/Tests/elevator_button_input_test.c
+++ src/Tests/elevator_button_input_test.c
@@ -5,7 +5,7 @@
 #include "../inc/elevator_control/elevator_button_inputs.h"
 #include "../inc/elevator_hardware/elevator_hardware.h"
 
-#define UDELAY 2000 * 1000 // 2000 ms
+#define UDELAY 100 * 1000 // 2000 ms
 
 void *PollOrders(){
     elevator_hardware_info_t elevator_1;
@@ -17,9 +17,11 @@ void *PollOrders(){
 
     order_queue_t *queue = create_order_queue(10);
     while(1){
-        poll_new_orders(&elevator_1, queue);
-        for(int i = 0; i < queue->size; i++){
-            printf("%d \t Order ID: %d\n", i , queue->orders[i].order_id);
+        if (poll_new_orders(&elevator_1, queue)){
+            for(int i = 0; i < queue->size; i++){
+                printf("%d \t Order ID: %d\n", i , queue->orders[i].order_id);
+            }
+            printf("\n");
         }
         usleep(UDELAY);
     }
