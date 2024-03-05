238a8353ab21c4b3c24094bdff5e4c1fbdf43622
Trond F. Christiansen <trochr@hotmail.com>
2024-03-05 11:10:07 +0100

    Started que sunc integration

--- src/Tests/elevator_button_input_test.c
+++ src/Tests/elevator_button_input_test.c
@@ -4,8 +4,18 @@
 
 #include "../inc/elevator_control/elevator_button_inputs.h"
 #include "../inc/elevator_hardware/elevator_hardware.h"
+#include "../inc/order_queue/orderQueue.h"
+#include "../inc/order_queue/send_order_queue.h"
 
 #define UDELAY 100 * 1000 // 2000 ms
+
+order_queue_t* queue;
+
+/* Callback function for tcp connupdate*/
+void connectionStatus(const char * ip, int status){
+
+  printf("A connection got updated %s: %d\n",ip,status);
+}
 
 void *PollOrders(){
     elevator_hardware_info_t elevator_1;
@@ -15,7 +25,7 @@ void *PollOrders(){
     elevator_hardware_init(&elevator_1);
 
 
-    order_queue_t *queue = create_order_queue(10);
+    
     while(1){
         if (poll_new_orders(&elevator_1, queue)){
             for(int i = 0; i < queue->size; i++){
@@ -29,12 +39,32 @@ void *PollOrders(){
     return NULL;
 }
 
+void* SyncOrders(){
+
+    send_order_queue_init(NULL,connectionStatus);
+    send_order_queue_connect("127.0.0.1",9000);
+    while(1){
+        pthread_mutex_lock(queue->queue_mutex);
+        send_order_queue_send_order("127.0.0.1",queue);
+        pthread_mutex_unlock(queue->queue_mutex);
+        usleep(500*1000);
+    }
+    return NULL;
+}
+
 int main(){
+    queue = create_order_queue(10);
+
+    
 
     pthread_t pollOrders;
+    pthread_t syncOrders;
+
     pthread_create(&pollOrders, NULL, PollOrders, NULL);
+    pthread_create(&syncOrders, NULL, SyncOrders, NULL);
 
     pthread_join(pollOrders, NULL);
+    pthread_join(syncOrders, NULL);
 
     return 0;
 
--- src/inc/order_queue/orderQueue.c
+++ src/inc/order_queue/orderQueue.c
@@ -5,6 +5,7 @@ order_queue_t * create_order_queue(int capacity){
     queue->capacity = capacity;
     queue->size = 0;
     queue->orders = (order_event_t *)malloc(queue->capacity * sizeof(order_event_t));
+    queue->queue_mutex = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t));
     pthread_mutex_init(queue->queue_mutex, NULL);
     return queue;
 }
