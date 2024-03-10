c44965c358b64575f975e0341bd3ebfd0a74c78d
Peter Knutsen <peterk98@gmail.com>
2024-03-10 22:25:50 +0100

    orderQueueTest fixed: %d replaced with %ld

--- src/Tests/orderQueueTest.c
+++ src/Tests/orderQueueTest.c
@@ -33,11 +33,11 @@ void test_enqueue_order(order_queue_t * queue){
     queue->capacity = 10;
     queue->size = 0;
     enqueue_order(queue, &order);
-    printf("order IDs: %d, %d, %d\n", queue->orders[0].order_id, queue->orders[1].order_id, queue->orders[2].order_id);
+    printf("order IDs: %ld, %ld, %ld\n", queue->orders[0].order_id, queue->orders[1].order_id, queue->orders[2].order_id);
     enqueue_order(queue, &order1);
-    printf("order IDs: %d, %d, %d\n", queue->orders[0].order_id, queue->orders[1].order_id, queue->orders[2].order_id);
+    printf("order IDs: %ld, %ld, %ld\n", queue->orders[0].order_id, queue->orders[1].order_id, queue->orders[2].order_id);
     enqueue_order(queue, &order2);
-    printf("order IDs: %d, %d, %d\n", queue->orders[0].order_id, queue->orders[1].order_id, queue->orders[2].order_id);
+    printf("order IDs: %ld, %ld, %ld\n", queue->orders[0].order_id, queue->orders[1].order_id, queue->orders[2].order_id);
 
     printf("Queue size: %d\n", queue->size);
     assert(queue->size == 3);
@@ -79,7 +79,7 @@ void test_enq_dq_order(order_queue_t * queue){
     assert(queue->orders[0].order_id == 1);
     assert(queue->orders[1].order_id == 2);
     assert(queue->orders[2].order_id == 3);
-    printf("enqueued order IDs: %d, %d, %d\n", queue->orders[0].order_id, queue->orders[1].order_id, queue->orders[2].order_id);
+    printf("enqueued order IDs: %ld, %ld, %ld\n", queue->orders[0].order_id, queue->orders[1].order_id, queue->orders[2].order_id);
 
     printf("Testing enqueue of existing order\n");
     enqueue_order(queue, &order);
@@ -90,7 +90,7 @@ void test_enq_dq_order(order_queue_t * queue){
     dequeue_order(queue, &order1);
     assert(queue->size == 1);
     assert(queue->orders[0].order_id == 3);
-    printf("Post dequeue order IDs: %d\n", queue->orders[0].order_id);
+    printf("Post dequeue order IDs: %ld\n", queue->orders[0].order_id);
 }
 
 void test_create_order_queue(){
@@ -113,35 +113,35 @@ int main(){
     order.floor = 2;
     order.order_type = GO_TO;
     order.order_id = GenerateOrderID(&order);
-    printf("Order 1 ID: %d\n", order.order_id);
+    printf("Order 1 ID: %ld\n", order.order_id);
 
     order_event_t order2;
     strcpy(order2.elevator_id, "127.0.0.1");
     order2.floor = 2;
     order2.order_type = DOWN_FROM;
     order2.order_id = GenerateOrderID(&order2);
-    printf("Order 2 ID: %d\n", order2.order_id);
+    printf("Order 2 ID: %ld\n", order2.order_id);
 
     order_event_t order3;
     strcpy(order3.elevator_id, "127.0.0.1");
     order3.floor = 3;
     order3.order_type = DOWN_FROM;
     order3.order_id = GenerateOrderID(&order3);
-    printf("Order 3 ID: %d\n", order3.order_id);
+    printf("Order 3 ID: %ld\n", order3.order_id);
 
     order_event_t order4;
     strcpy(order4.elevator_id, "127.0.0.1");
     order4.floor = 1;
     order4.order_type = UP_FROM;
     order4.order_id = GenerateOrderID(&order4);
-    printf("Order 4 ID: %d\n", order4.order_id);
+    printf("Order 4 ID: %ld\n", order4.order_id);
 
     order_event_t order5;
     strcpy(order5.elevator_id, "127.0.0.1");
     order5.floor = 1;
     order5.order_type = UP_FROM;
     order5.order_id = GenerateOrderID(&order5);
-    printf("Order 5 ID: %d\n", order5.order_id);
+    printf("Order 5 ID: %ld\n", order5.order_id);
     /*  */
 
     /* Testing enqueueing */
