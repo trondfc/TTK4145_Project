7ad95a62bffcc514408de505b33fb30aa6b412c3
Syver <Syverharaldsen@gmail.com>
2024-03-15 10:05:30 +0100

    Added prototypes to elevator_algorithm.h

--- src/inc/elevator_algorithm/elevator_algorithm.h
+++ src/inc/elevator_algorithm/elevator_algorithm.h
@@ -7,3 +7,11 @@
 #include "../elevator_hardware/elevator_hardware.h"
 #include "../order_queue/orderQueue.h"
 #include "../config.h"
+
+/* Prototypes */
+bool order_in_elevator_path(order_event_t* order, elevator_status_t* elevator);
+bool elevator_has_reserved_orders(order_queue_t* queue, elevator_status_t* elevator);
+bool reserved_order_at_position(order_queue_t* queue, elevator_status_t* elevator);
+long find_closest_order(order_queue_t* queue, elevator_status_t* elevator);
+elevator_state_t direction_to_order(order_event_t* order, elevator_status_t* elevator);
+
