87a197a0851e12df645e1445319df62c66de4061
Trond F. Christiansen <trochr@hotmail.com>
2024-03-17 01:00:51 +0100

    Add at_floor flag to elevator struct

--- src/inc/elevator_control/elevator_autofind.c
+++ src/inc/elevator_control/elevator_autofind.c
@@ -21,6 +21,7 @@ elevator_status_t* elevator_struct_init(){
     strcpy(elevator[i].elevator.ip, "\0");
     elevator[i].alive = false;
     elevator[i].floor = 0;
+    elevator[i].at_floor = false;
     elevator[i].elevator_state = STOP;
     elevator[i].obstruction = false;
     elevator[i].emergency_stop = false;
